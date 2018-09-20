26efade376ea3da84dcf1008bf3a9a9f2b5eecf8
Alan Edel <alan3del@gmail.com>
2018-09-20 06:14:58 +0600

    hovers on map

--- kazinvest-front/src/assets/js/positron.js
+++ kazinvest-front/src/assets/js/positron.js
@@ -1,176 +1,265 @@
 export default () => ({
-    "version": 8,
-    "name": "Blank",
-    "metadata": {
-        "mapbox:autocomposite": true,
-        "mapbox:type": "template",
-        "mapbox:sdk-support": {
-            "js": "0.46.0",
-            "android": "6.0.0",
-            "ios": "4.0.0"
+    'version': 8,
+    'name': 'Blank',
+    'metadata': {
+        'mapbox:autocomposite': true,
+        'mapbox:type': 'template',
+        'mapbox:sdk-support': {
+            'js': '0.46.0',
+            'android': '6.0.0',
+            'ios': '4.0.0'
         }
     },
-    "center": [70.57477983415595, 52.86828778880209],
-    "zoom": 4.055863227793626,
-    "bearing": 0,
-    "pitch": 0,
-    "sources": {
-        "composite": {
-            "url": "mapbox://alanedel.cjm90ybsq0jsz2qps9bus0l61-7bc8n,alanedel.ark1w3xd,alanedel.dx9wk4j2,alanedel.cjm90zdb907gq2wmp3wsdp3m6-9iaf8,alanedel.cc7b6dd2,alanedel.cjm90wsdu2c0v32o7un0y4zh1-11pop,alanedel.cjm90whgh0xn02vnu9ni5nmre-09wbg,alanedel.cjm90ylwj2m9x32nxnwymzdih-0fgd2,alanedel.cjm90xhsi0y3s2vtn8jhfn5fb-59tpr,alanedel.cjm90x7qs2c1l2qo7twhzxid9-1fbbx,alanedel.cjm90z1se1ywa2vqvr1hzrdla-4wr07,alanedel.0yjs07oo,alanedel.cz505b5o,alanedel.2bdo6zvq",
-            "type": "vector"
+    'center': [70.57477983415595, 52.86828778880209],
+    'zoom': 4.055863227793626,
+    'bearing': 0,
+    'pitch': 0,
+    'sources': {
+        'openmaptiles': {
+            'type': 'vector',
+            'url': 'https://free.tilehosting.com/data/v3.json?key=hWWfWrAiWGtv68r8wA6D'
+        },
+        'composite': {
+            'url': 'mapbox://alanedel.cjm90ybsq0jsz2qps9bus0l61-7bc8n,alanedel.ark1w3xd,alanedel.dx9wk4j2,alanedel.cjm90zdb907gq2wmp3wsdp3m6-9iaf8,alanedel.cc7b6dd2,alanedel.cjm90wsdu2c0v32o7un0y4zh1-11pop,alanedel.cjm90whgh0xn02vnu9ni5nmre-09wbg,alanedel.cjm90ylwj2m9x32nxnwymzdih-0fgd2,alanedel.cjm90xhsi0y3s2vtn8jhfn5fb-59tpr,alanedel.cjm90x7qs2c1l2qo7twhzxid9-1fbbx,alanedel.cjm90z1se1ywa2vqvr1hzrdla-4wr07,alanedel.0yjs07oo,alanedel.cz505b5o,alanedel.2bdo6zvq',
+            'type': 'vector'
         }
     },
-    "sprite": "mapbox://sprites/alanedel/cjm8zxhak2s1i2sn29k3p7b8n",
-    "glyphs": "mapbox://fonts/alanedel/{fontstack}/{range}.pbf",
-    "layers": [
-        {
-            "id": "background",
-            "type": "background",
-            "layout": {},
-            "paint": {
-                "background-color": "hsl(0, 0%, 52%)",
-                "background-opacity": 0.03
+    'sprite': 'mapbox://sprites/alanedel/cjm8zxhak2s1i2sn29k3p7b8n',
+    'glyphs': 'https://free.tilehosting.com/fonts/{fontstack}/{range}.pbf?key=hWWfWrAiWGtv68r8wA6D',
+    'layers': [{
+            'id': 'background',
+            'type': 'background',
+            'layout': {},
+            'paint': {
+                'background-color': 'hsl(0, 0%, 52%)',
+                'background-opacity': 0.03
             }
         },
         {
-            "id": "background-pattern",
-            "type": "background",
-            "layout": {},
-            "paint": {
-                "background-color": "hsl(0, 0%, 52%)",
-                "background-opacity": 1,
-                "background-pattern": "Vector"
+            'id': 'background-pattern',
+            'type': 'background',
+            'layout': {},
+            'paint': {
+                'background-color': 'hsl(0, 0%, 52%)',
+                'background-opacity': 1,
+                'background-pattern': 'Vector'
             }
         },
         {
-            "id": "akm-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "akm_obl-b70enu",
-            "layout": {},
-            "paint": {
-                "fill-outline-color": "hsl(0, 0%, 100%)",
-                "fill-color": "#accad7"
+            'id': 'akm-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'akm_obl-b70enu',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
             }
         },
         {
-            "id": "akt-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "akt_obl-0or7p0",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "alm-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "alm_obl-07xgak",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "vko-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "vko",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "tur-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "tur_obl",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "sko-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "sko",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "pavl-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "pavl_obl",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "kyz-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "kyz_obl",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "kost-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "kost_obl",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "kar-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "kar_obl",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "jamb-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "jamb_obl",
-            "layout": {},
-            "paint": {"fill-outline-color": "#ffffff", "fill-color": "#accad7"}
-        },
-        {
-            "id": "man-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "man_obl-9taiot",
-            "layout": {},
-            "paint": {
-                "fill-outline-color": "hsl(0, 0%, 100%)",
-                "fill-color": "#accad7"
+            'id': 'akt-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'akt_obl-0or7p0',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
             }
         },
         {
-            "id": "zko-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "zko_obl-8qi6mt",
-            "layout": {},
-            "paint": {
-                "fill-outline-color": "hsl(0, 0%, 100%)",
-                "fill-color": "#accad7"
+            'id': 'alm-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'alm_obl-07xgak',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
             }
         },
         {
-            "id": "atyr-obl",
-            "type": "fill",
-            "source": "composite",
-            "source-layer": "atyr_obl-3y40uz",
-            "layout": {},
-            "paint": {
-                "fill-outline-color": "hsl(0, 0%, 100%)",
-                "fill-color": "#accad7"
+            'id': 'vko-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'vko',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
             }
-        }
+        },
+        {
+            'id': 'tur-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'tur_obl',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'sko-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'sko',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'pavl-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'pavl_obl',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'kyz-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'kyz_obl',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'kost-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'kost_obl',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'kar-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'kar_obl',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'jamb-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'jamb_obl',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'man-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'man_obl-9taiot',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'zko-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'zko_obl-8qi6mt',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
+        {
+            'id': 'atyr-obl',
+            'type': 'fill',
+            'source': 'composite',
+            'source-layer': 'atyr_obl-3y40uz',
+            'layout': {},
+            'paint': {
+                'fill-outline-color': '#ffffff',
+                'fill-color': ['case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    '#03A0E3',
+                    '#accad7',
+                ],
+            }
+        },
     ],
-    "created": "2018-09-19T10:20:07.260Z",
-    "id": "cjm8zxhak2s1i2sn29k3p7b8n",
-    "modified": "2018-09-19T12:45:34.956Z",
-    "owner": "alanedel",
-    "visibility": "public",
-    "draft": false
-  });
+    'created': '2018-09-19T10:20:07.260Z',
+    'id': 'cjm8zxhak2s1i2sn29k3p7b8n',
+    'modified': '2018-09-19T12:45:34.956Z',
+    'owner': 'alanedel',
+    'visibility': 'public',
+    'draft': false
+});
--- kazinvest-front/src/components/Map.vue
+++ kazinvest-front/src/components/Map.vue
@@ -1,54 +1,264 @@
 <template>
-    <div id="map">
+    <div id='map'>
     </div>
 </template>
 
 <script>
-    import mapboxgl from 'mapbox-gl'
-    import positron from '../assets/js/positron'
-    import '../assets/css/mapbox-gl.css'
-    import {mapGetters} from 'vuex'
+import mapboxgl from "mapbox-gl";
+import positron from "../assets/js/positron";
+import "../assets/css/mapbox-gl.css";
+import { mapGetters } from "vuex";
 
-    export default {
-        name: "mapgl",
-        mounted() {
-            mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhbmVkZWwiLCJhIjoiY2pnZXhxN2h4NGZjdzJ3a2w5bHQxcDcxcyJ9.74qnkPsP7PUJ4Fu3Jv5LuQ';
-            this.map = new mapboxgl.Map({
-                container: 'map',
-                style: positron(),
-                // style: 'mapbox://styles/alanedel/cjm8zxhak2s1i2sn29k3p7b8n',
-                zoom: 5,
-                // maxBounds: [[46.5857415370,37.6890455714], [86.1365227870,60.2212701884]],
-                center: [66.6751886295297, 48.0219296299588],
-                // attributionControl: false,
-                interactive: false,
-            });
+export default {
+  name: "mapgl",
+  data() {
+    return {
+      hoveredStateId: null,
+      hoveredFeature: null
+    };
+  },
+  mounted() {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYWxhbmVkZWwiLCJhIjoiY2pnZXhxN2h4NGZjdzJ3a2w5bHQxcDcxcyJ9.74qnkPsP7PUJ4Fu3Jv5LuQ";
+    this.map = new mapboxgl.Map({
+      container: "map",
+      style: positron(),
+      zoom: 4,
+      center: [62.0672845, 48.3718379],
+      interactive: false
+    });
 
-            this.map.once('style.load', _ => {
-                // this._addObjects();
-            });
+    this.map.once("style.load", _ => {
+      // this._addObjects();
+    });
 
-        },
-        computed: {
-            // ...mapGetters({
-            //     geometries: 'getGeometries',
-            // }),
-        },
-        methods: {
-            
+    this.map.on("mousemove", e => {
+      var features = this.map.queryRenderedFeatures(e.point);
+      if (features[0]) {
+        if (this.hoveredStateId) {
+          this.map.setFeatureState(
+            {
+              source: "composite",
+              id: this.hoveredStateId,
+              sourceLayer: features[0].sourceLayer
+            },
+            { hover: false }
+          );
         }
-    }
+        this.hoveredStateId = features[0].id;
+        this.hoveredFeature = features[0];
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: features[0].sourceLayer
+          },
+          { hover: true }
+        );
+      }
+    });
+
+    this.map.on("mouseleave", "akm-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "akm_obl-b70enu"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "akt-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "akt_obl-0or7p0"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "alm-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "alm_obl-07xgak"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "vko-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "vko"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "tur-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "tur_obl"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "sko-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "sko"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "pavl-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "pavl_obl"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "kyz-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "kyz_obl"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "kost-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "kost_obl"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "kar-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "kar_obl"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "jamb-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "jamb_obl"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "man-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "man_obl-9taiot"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "zko-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "zko_obl-8qi6mt"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+    this.map.on("mouseleave", "atyr-obl", _ => {
+      if (this.hoveredStateId) {
+        this.map.setFeatureState(
+          {
+            source: "composite",
+            id: this.hoveredStateId,
+            sourceLayer: "atyr_obl-3y40uz"
+          },
+          { hover: false }
+        );
+      }
+      this.hoveredStateId = null;
+    });
+  },
+  computed: {
+    // ...mapGetters({
+    //     geometries: 'getGeometries',
+    // }),
+  },
+  methods: {}
+};
 </script>
 
 <style>
-    .mapboxgl-map {
-        height: 100%;
-        width: 100%;
-        position: absolute; 
-        top: 0; 
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #fff;
-    }
+.mapboxgl-map {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+}
 </style>
