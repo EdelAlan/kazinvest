32a85063b74cfdbb83d08632cb098272078ebdeb
Ruslan Timurziyev <r.timurziyev@kagis.kz>
2018-09-26 15:46:39 +0600

    modal, some changes

--- kazinvest-front/src/main.js
+++ kazinvest-front/src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from  './store'
+import Vue from 'vue';
+import app from './app.vue';
+import router from './router';
+import store from  './components/store/_all';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(app),
 }).$mount('#app');
--- kazinvest-front/src/router.js
+++ kazinvest-front/src/router.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/Home.vue'
+
+import home from './components/pages/home.vue'
 
 Vue.use(Router);
 
 export default new Router({
-    hashbang: false,
-    history: true,
-    mode: 'history',
-    linkActiveClass: 'active',
-    transitionOnLoad: true,
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        
-    ]
+	hashbang: false,
+	history: true,
+	mode: 'history',
+	linkActiveClass: 'active',
+	transitionOnLoad: true,
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: home
+		},
+	]
 })
