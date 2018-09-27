e939c4feec3f6506bceaeeabf23c0ff0226b5210
Alan Edel <alan3del@gmail.com>
2018-09-27 16:27:57 +0600

    создал store для компонента map

--- kazinvest-front/src/components/store/_all.js
+++ kazinvest-front/src/components/store/_all.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 
 import signup from './signup';
 import ui_visibility from './ui_visibility';
+import map from './map';
 
 Vue.use(Vuex);
 
@@ -10,5 +11,6 @@ export default new Vuex.Store({
 	modules: {
 		signup,
 		ui_visibility,
+		map,
 	},
 });
