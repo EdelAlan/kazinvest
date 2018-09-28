da1f6f64a8c38a95d3507ec0dc047b64fbe9aa2a
Alan Edel <alan3del@gmail.com>
2018-09-28 17:48:04 +0600

    подключение в боевой базе

--- kazinvest-back/index.js
+++ kazinvest-back/index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const controller = require('./src/api/map');
 
-process.env.PGUSER = 'postgres';
-process.env.PGPASSWORD = '1';
-process.env.PGDATABASE = 'alan';
-process.env.PGPORT = '5432';
-
 var cross = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
